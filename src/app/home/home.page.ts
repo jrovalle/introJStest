c783b31b0cd40e4a9dc804a5d44b2064a37d9882
Jafet Benitez <jafetg.benitez@gmail.com>
2021-02-02 11:14:00 -0400

    Implementation of introjs with steps in code-behind

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,17 +3,35 @@ import * as introJs from 'intro.js/intro.js';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
 
   constructor() {}
   showHelp(){
-    introJs(document.querySelector("app-home")).setOptions({
-      'nextLabel': 'Next step',
-      'prevLabel': 'Previous step',
-      'skipLabel': 'Don\'t bother me!',
-      'doneLabel': 'Finish'
-    }).start(); 
+    // introJs(document.querySelector('app-home'))
+    // // .setOptions({
+    // //   nextLabel: 'Next',
+    // //   prevLabel: 'Prev.',
+    // //   skipLabel: 'Skip',
+    // //   doneLabel: 'Finish'
+    // // })
+    // .start();
+
+    introJs().setOptions({
+      tooltipClass: 'eetz-tooltip',
+      steps: [{
+        title: this.html('intro'),
+        intro: this.html('Hello world!')
+      }, {
+        title: this.html('Stp 2') ,
+        element: document.querySelector('#container'),
+        intro: this.html('Click here to login!')
+      }]
+    }).start();
+  }
+
+  private html(text: string) {
+    return `<span style="color:black">${text}<span>`;
   }
 }
